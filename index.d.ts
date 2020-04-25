69c0df57cd57659ae6a6d51473a557db63d14be8
zhouyk <zhouyunkui@bytedance.com>
2020-04-25 14:10:06 +0800

    mod(index.d.ts): 始终以初始值的类型为准

--- index.d.ts
+++ index.d.ts
@@ -3,15 +3,15 @@ import { Femo } from "./src/interface";
 // gluer
 export type HandleFunc<S, D, CR = Partial<S> | Promise<Partial<S>>> = (data: D, state: S) => CR;
 
-type fnc<S, R> = <D = Partial<S>, CR = R>(data: D, customHandler?: HandleFunc<S, D, CR>) => CR;
+type fnc<S, R> = <D = Partial<S>, CR = R>(data: D, customHandler?: HandleFunc<S, D>) => CR;
 export type GluerReturn<S, R>  = {
   readonly [P in keyof S]: S[P];
 } & fnc<S, R> & {
   actionType: string
 };
 
-export function gluer<S, D = S, R = Partial<S>>(onlyOne: HandleFunc<S, D, R> | S) : GluerReturn<S, R>;
-export function gluer<S, D = S, R = Partial<S>>(fn:  HandleFunc<S, D, R>, initialState: S) : GluerReturn<S, R>;
+export function gluer<S = any, D = S, R = Partial<S>>(onlyOne: HandleFunc<S, D, R> | S) : GluerReturn<S, R>;
+export function gluer<S = any, D = S, R = Partial<S>>(fn:  HandleFunc<S, D, R>, initialState: S) : GluerReturn<S, R>;
 export default function femo<T>(structure: T): Femo<T>;
 
 export { Connect, ConnectPlugin, ConnectRegister, Femo } from './src/interface';
