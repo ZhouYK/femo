5584066c6d2387854b32396cecf1ae4bb01007ee
zhouyunkui <zhouyunkui@admindeMacBook-Pro.local>
2021-03-03 14:51:26 +0800

    feat: 模型更新数据，返回的数据类型统一为预定义的state类型
    以前处于灵活性的考虑，当出现这种情况时 cont name = gluer('小明') 预定义的state是字符串；const returnData = name(1) returnData类型为any；实际上returnData的值为1。这种直接更新任意类型数据，没有任何的函数做处理的都会直接替换掉整个state。类型预期应该和初始定义的类型是一致。也就是说returnData提示的类型应该为字符串类型，只是具体的值是什么类型由开发者自己负责。（这里由于没办法知道传入单个值的时候后续是否有函数处理，只能做到类型一致，实际的值没办法保证。目前还不能在运行的时候利用ts的类型做判断，因为只有运行的时候才知道传入的数据是什么，所以开发者一定要记住每个数据一旦定义了类型就不能变了，在没有任何函数处理传入的单个数据参数的情况下一定要小心一点）

--- index.d.ts
+++ index.d.ts
@@ -15,10 +15,10 @@ export type HandleFunc<S, D, CR> = (data: D, state: S) => CR;
 
 export type GluerReturnFn<S, R> = {
   (): S;
-  <D = Partial<S>, CR = R>(data: D): CR;
-  <D = Partial<S>>(data: D, customHandler: HandleFunc<S, D, Partial<S>>): Partial<S>;
-  <D = undefined>(customHandler: HandleFunc<S, D, Partial<S>>): Partial<S>;
   <D = undefined>(customHandler: HandleFunc<S, D, Promise<Partial<S>>>): Promise<Partial<S>>;
+  <D = undefined>(customHandler: HandleFunc<S, D, Partial<S>>): Partial<S>;
+  <D = Partial<S>>(data: D): S;
+  <D = Partial<S>>(data: D, customHandler: HandleFunc<S, D, Partial<S>>): Partial<S>;
   <D = Partial<S>>(data: D, customHandler: HandleFunc<S, D, Promise<Partial<S>>>): Promise<Partial<S>>;
 }
 
