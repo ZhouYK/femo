3cb3d9418291eff5c35d72d705c2f84564920792
zhouyunkui <zhouyunkui@admindeMacBook-Pro-4.local>
2021-05-19 15:34:26 +0800

    fix: 增加导出 useDerivedModel

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as genRaceQueue } from './genRaceQueue';
 export { default as useModel } from './hooks/useModel';
 export { default as useIndividualModel } from './hooks/useIndividualModel';
 export { default as useDerivedStateToModel } from './hooks/useDerivedStateToModel';
+export { default as useDerivedModel } from './hooks/useDerivedModel';
