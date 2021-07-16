5c79c415fca5947b58e7ee5b59df999abc3e5fcf
zhouyunkui <zhouyunkui@admindeMacBook-Pro-4.local>
2021-07-16 15:26:34 +0800

    fix: suspense每次触发都将组件状态回滚到出发之前的那一次render

--- src/hooks/internalHooks/useService.ts
+++ src/hooks/internalHooks/useService.ts
@@ -1,16 +1,21 @@
-import {useRef} from "react";
+import {useState} from "react";
 import {GluerReturn, Service, ServiceOptions} from "../../../index";
 import {isAsync} from "../../tools";
+import gluer, {defaultReducer} from "../../gluer";
 
 const useService = <T>(model: GluerReturn<T>, deps?: [Service<T>], options?: ServiceOptions) => {
   const [service] = deps || [];
   const { suspense } = options || {};
-  const firstRenderFlagRef = useRef(true);
-  const serviceCacheRef = useRef(service);
+  const [firstRenderFlagRef] = useState(() => {
+    return gluer(true);
+  });
+  const [serviceCacheRef] = useState(() => {
+    return gluer(defaultReducer , service)
+  });
 
-  if (service && firstRenderFlagRef.current) {
-    firstRenderFlagRef.current = false;
-    const result = service(model());
+  if (serviceCacheRef() && firstRenderFlagRef()) {
+    firstRenderFlagRef.silent(false);
+    const result = (serviceCacheRef() as Service<T>)(model());
     if (isAsync(result)) {
       if (suspense) throw model.race(() => result);
       // 这里更新了loading，会跳过当次渲染
@@ -20,10 +25,10 @@ const useService = <T>(model: GluerReturn<T>, deps?: [Service<T>], options?: Ser
     }
   }
 
-  if (!Object.is(serviceCacheRef.current, service)) {
-    serviceCacheRef.current = service;
+  if (!Object.is(serviceCacheRef(), service)) {
+    serviceCacheRef.silent(() => service);
     if (service) {
-      const result = service(model());
+      const result = (serviceCacheRef() as Service<T>)(model());
       if (isAsync(result)) {
         if (suspense) throw model.race(() => result);
         model.race(() => result);
