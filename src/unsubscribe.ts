37668c1c3668d6ad2cac5b0413dfe40fc8e564f2
zhouyunkui <zhouyunkui@admindeMacBook-Pro-4.local>
2022-03-24 16:32:25 +0800

    update: 优化代码

--- src/unsubscribe.ts
+++ src/unsubscribe.ts
@@ -17,27 +17,22 @@ const unsubscribe = (targetDeps?: GluerReturn<any>[], callback?: Callback) => {
   if (targetDeps === undefined && callback === undefined) {
     refToDepsMap.clear();
     depsToFnMap.clear();
-  } else {
-    if (!Object.is(targetDeps, undefined) && !Object.is(callback, undefined)) {
-      if (typeof callback !== 'function') {
-        console.warn('unsubscribe`s param: callback, should be function')
-      }
-      const fns = (depsToFnMap.get(targetDeps as GluerReturn<any>[]) || new Set()) as Set<Callback>;
-      fns.delete(callback as Callback);
-      if (fns.size !== 0) {
-        return;
-      }
-    }
+    return;
+  }
 
-    if (Object.is(targetDeps, undefined) && !Object.is(callback, undefined)) {
-      // todo 将来再加这个的解除功能吧
-      console.warn('unsubscribe`s param: targetDeps, should not be undefined');
+  if (!Object.is(targetDeps, undefined) && !Object.is(callback, undefined)) {
+    const fns = (depsToFnMap.get(targetDeps as GluerReturn<any>[]) || new Set()) as Set<Callback>;
+    fns.delete(callback as Callback);
+    if (fns.size !== 0) {
       return;
     }
-
-    depsToFnMap.delete(targetDeps as GluerReturn<any>[]);
-    deleteDepsInRefToDepsMap(targetDeps as GluerReturn<any>[]);
+  } else if (Object.is(targetDeps, undefined) && !Object.is(callback, undefined)) {
+    // todo 将来再加这个的解除功能吧
+    console.warn('unsubscribe`s param: targetDeps, should not be undefined');
+    return;
   }
+  depsToFnMap.delete(targetDeps as GluerReturn<any>[]);
+  deleteDepsInRefToDepsMap(targetDeps as GluerReturn<any>[]);
 }
 
 export default unsubscribe;
